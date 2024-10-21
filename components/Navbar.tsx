70c89fc123fb656e5a8756a42a0b0f8e16aa00a6
kaoutar-12 <kmouradi@student.1337.ma>
2024-10-21 20:13:05 +0100

    done with navbar

---
+++ components/Navbar.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import "../styles/navbar.css";
+import { CgSearch } from "react-icons/cg";
+import { FaHeart } from "react-icons/fa";
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <div className="logo">
+        <span>CineVerse</span>
+      </div>
+      <div className="search">
+        <div className="search-inside">
+          <input type="text" placeholder="Search" />
+          <CgSearch className="icon" />
+        </div>
+      </div>
+      <div className="list">
+        <FaHeart className="icon-list" />
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
