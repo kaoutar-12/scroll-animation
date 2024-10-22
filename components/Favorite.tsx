ec61869349c2b60327d3ad3dcd42f29f293b62b2
kaoutar-12 <kmouradi@student.1337.ma>
2024-10-22 23:11:36 +0100

    last commit

---
+++ components/Favorite.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "@/styles/favorite.css";
+
+type Props = {}
+
+const Favorite = (props: Props) => {
+  return (
+    <div className="fav"></div>
+  )
+}
+
+export default Favorite
