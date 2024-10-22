1e4e735c87f3698fa8ad5cf62a509ff5d57972c0
kaoutar-12 <kmouradi@student.1337.ma>
2024-10-22 12:56:05 +0100

    finished infinity scroll and working on make each one have specific speed scrolling

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import Navbar from "../components/Navbar";
+import Slider from "../components/Slider";
 
 export default function Home() {
   return (
     <div >
-     <Navbar/>
+     {/* <Navbar/> */}
+     <Slider/>
     </div>
   );
 }
---
+++ components/Slider.tsx
@@ -0,0 +1,87 @@
+
+
+"use client";
+
+import React, { useEffect, useRef } from "react";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import "@/styles/slider.css";
+
+// Register ScrollTrigger plugin
+gsap.registerPlugin(ScrollTrigger);
+
+interface smallPartsProps {
+  smallParts: string[];
+}
+
+const Tst: React.FC<smallPartsProps> = ({ smallParts }) => {
+  return (
+    <>
+      {smallParts.map((_, smallIndex) => (
+        <li className="small-part" key={smallIndex}></li>
+      ))}
+    </>
+  );
+};
+
+const Slider = () => {
+  const bigParts = Array(4).fill(null);
+  const smallParts = Array(20).fill(null);
+
+  // Use refs to keep track of big parts for ScrollTrigger animations
+  const bigPartRefs = useRef<Array<HTMLUListElement | null>>([]);
+
+  useEffect(() => {
+    // Infinite scroll logic for each big part
+    bigPartRefs.current.forEach((bigPart, index) => {
+      if (bigPart) {
+        ScrollTrigger.create({
+          trigger: bigPart,
+          start: 0,
+          end: "max",
+          onLeave: (self) => {
+            self.scroll(1); // Scroll back to the top
+            ScrollTrigger.update();
+          },
+          onLeaveBack: (self) => {
+            self.scroll(ScrollTrigger.maxScroll(window) - 1); // Scroll back to the bottom
+            ScrollTrigger.update();
+          }
+        });
+
+        // Animation for each small-part inside the big-part
+        const smallParts = bigPart.querySelectorAll(".small-part");
+        smallParts.forEach((part) => {
+          gsap.to(part, {
+            opacity: 1,
+            repeat: 1,
+            yoyo: true,
+            ease: "none",
+            scrollTrigger: {
+              trigger: part,
+              start: "center bottom",
+              end: "center top",
+              scrub: true,
+            },
+          });
+        });
+      }
+    });
+  }, []);
+
+  return (
+    <div className="container">
+      {bigParts.map((_, bigIndex) => (
+        <ul
+        className={`big-part part${bigIndex }`}
+          key={bigIndex}
+          ref={(el) => (bigPartRefs.current[bigIndex] = el)}
+        >
+          <Tst smallParts={smallParts} />
+        </ul>
+      ))}
+    </div>
+  );
+};
+
+export default Slider;
