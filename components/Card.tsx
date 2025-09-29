90db7dcf78ff13234e72b7c986f3ab412c4ca143
kaoutar-12 <kmouradi@student.1337.ma>
2025-09-29 10:57:17 +0100

    refresh animation

--- components/Card.tsx
+++ components/Card.tsx
@@ -3,36 +3,43 @@ import { MovieResult } from "./Slider";
 import Image from "next/image";
 import "../styles/card.css";
 import { useMovieContext } from "@/context/MovieContext";
+
 interface CardProps {
   movie: MovieResult;
-  onCardClick?: (movie: MovieResult) => void; // Optional click handler
+  onCardClick?: (movie: MovieResult) => void;
 }
 
 const Card: React.FC<CardProps> = ({ movie, onCardClick }) => {
-
-const {isLoading}= useMovieContext();
+  const { isLoading } = useMovieContext();
 
   return (
     <div className={`card ${isLoading ? "loading" : ""}`} onClick={() => onCardClick && onCardClick(movie)}>
-      {movie.poster_path ? (
-        <Image
-          src={`https://image.tmdb.org/t/p/w300${movie.poster_path}`}
-          alt={movie.title}
-          loading="lazy"
-          width={350}
-          height={500}
-          
-        />
-      ) : (
-        <Image
-          src={"/placeholder.png"}
-          alt={movie.title}
-          loading="lazy"
-          fill
-         
-        />
-      )}
-      {/* <h3>{movie.title}</h3> */}
+      <div className="card-inner">
+        {/* Front */}
+        <div className="card-front">
+          {movie.poster_path ? (
+            <Image
+              src={`https://image.tmdb.org/t/p/w300${movie.poster_path}`}
+              alt={movie.title}
+              loading="lazy"
+              width={350}
+              height={500}
+            />
+          ) : (
+            <Image src={"/placeholder.png"} alt={movie.title} loading="lazy" fill />
+          )}
+        </div>
+
+        {/* Back */}
+        <div className="card-back">
+          <Image
+            src={"/card-back.png"} // replace with your back image
+            alt="back"
+            fill
+            style={{ objectFit: "cover" }}
+          />
+        </div>
+      </div>
     </div>
   );
 };
